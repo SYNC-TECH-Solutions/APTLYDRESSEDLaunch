eace20ace3b91a8b94f4f699c5f4e951affd6872
SYNC TECH Solutions <synctechire@gmail.com>
2025-09-30 14:08:46 +0000

    lovely. now lets fix the newsletter form and then we should be good to g

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,7 +6,7 @@ const firebaseConfig = {
   "appId": "1:612499069843:web:9ea95553b5b5b9d1452666",
   "apiKey": "AIzaSyDNE90A2RNRkumIyNMA71ZVB-YGvQ2SVYk",
   "authDomain": "studio-7558384587-98caa.firebaseapp.com",
-  "storageBucket": "studio-7558384587-98caa.appspot.com",
+  "storageBucket": "studio-7558384587-98caa.firebasestorage.app",
   "messagingSenderId": "612499069843",
   "measurementId": "G-K3L3E715TR"
 };
